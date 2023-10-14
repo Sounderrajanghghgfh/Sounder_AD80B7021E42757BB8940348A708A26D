e5af79f0915e77304eca2b018c4603ed4c694079
Sounderrajanghghgfh <144306080+Sounderrajanghghgfh@users.noreply.github.com>
2023-10-14 08:54:45 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def fact(n):
- if n==0 or n==1:
-   return 1
- else:
-   return n*(fact(n-1))
-print(fact(5))
+class Player:
+
+  def play(self):
+    print("The player is  playing cricket.")
+
+
+#Define the deriver class Bataman
+class Batsman(Player):
+
+  def play(self):
+    print("The batsman is batting.")
+
+
+#Define the derived class Bowter
+class Bowler(Player):
+
+  def play(self):
+    print("The bowler is bowling.")
+
+
+#Create dbjocts of Bausnen and Doul
+batsman = Batsman()
+bowler = Bowler()
+batsman.play()
+bowler.play()
